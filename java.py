87b04da6c8de7f1ec01d85adb2c3bc3fcb5935b5
justfoolingaround <kr.justfoolingaround@gmail.com>
2023-01-18 18:02:26 +0545

    Chore: Initial commit

---
+++ java.py
@@ -0,0 +1,200 @@
+import asyncio
+import os
+import time
+from collections import defaultdict, deque
+
+import aiohttp
+import dotenv
+
+dotenv.load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+assert TOKEN, "TOKEN environment variable is not set."
+
+CAPABILITIES = 1 << 9
+
+
+DISCORD_WS_URL = "wss://gateway.discord.gg/"
+DISCORD_VERSION = 9
+DISCORD_API_URL = f"https://discord.com/api/v{DISCORD_VERSION}/"
+CLIENT_PROPERTIES = {
+    "os": "Windows",
+    "browser": "Discord Client",
+    "release_channel": "stable",
+    "client_version": "1.0.9008",
+    "os_version": "10.0.22621",
+    "os_arch": "x64",
+    "system_locale": "en-US",
+}
+
+WAIT_TIME = 300.0
+JAVA_DEVELOPERS = [] # ["user_id, in string"]
+MESSAGE = "https://cdn.discordapp.com/emojis/531182905851117588.png?size=96"
+
+
+class TheJavaBully:
+    def __init__(
+        self,
+        session: aiohttp.ClientSession,
+        token,
+        message,
+        wait_time=0.0,
+        exempt_channels=None,
+        targets=None,
+        *,
+        reply=False,
+    ):
+        self.session = session
+        self.token = token
+        self.wait_time = wait_time
+
+        self.targets = targets or list()
+        self.targets_channels = defaultdict(asyncio.Event)
+        self.exempt_channels = exempt_channels or list()
+
+        self.channel_message_store = defaultdict(deque)
+
+        self.message = message
+        self.reply = reply
+
+    async def watch(self, channel_id):
+        if channel_id in self.targets_channels or channel_id in self.exempt_channels:
+            return
+
+        event = self.targets_channels[channel_id]
+
+        until = time.time() + self.wait_time
+
+        while event.is_set() or time.time() < until:
+            await asyncio.sleep(1)
+
+        if event.is_set():
+            return
+
+        data = {
+            "content": self.message,
+        }
+
+        if self.reply and self.channel_message_store[channel_id]:
+            data["message_reference"] = {
+                "message_id": self.channel_message_store[channel_id][0]["id"],
+                "channel_id": channel_id,
+            }
+
+        await self.session.post(
+            DISCORD_API_URL + f"channels/{channel_id}/messages",
+            json=data,
+            headers={"Authorization": self.token},
+        )
+        await self.unwatch(channel_id)
+
+    async def unwatch(self, channel_id):
+        self.targets_channels[channel_id].set()
+        del self.targets_channels[channel_id]
+
+    async def on_message_create(self, data: dict):
+
+        channel_id = data["channel_id"]
+        self.channel_message_store[channel_id].appendleft(data)
+
+        author_id = data["author"]["id"]
+
+        if author_id not in self.targets:
+            if channel_id in self.targets_channels:
+                await self.unwatch(channel_id)
+
+            return
+
+        return await self.watch(channel_id)
+
+    async def on_message_delete(self, data: dict):
+        channel_id = data["channel_id"]
+        message_id = data["id"]
+
+        messages_deque = self.channel_message_store[channel_id]
+
+        for message in messages_deque.copy():
+            if message["id"] == message_id:
+                messages_deque[channel_id].remove(message)
+
+        if messages_deque and messages_deque[0]["author"]["id"] in self.targets:
+            return await self.watch(channel_id)
+
+        return await self.unwatch(channel_id)
+
+
+async def setup_heartbeat(ws: aiohttp.ClientWebSocketResponse, interval):
+    while not ws.closed:
+        await ws.send_json({"op": 1, "d": None})
+        await asyncio.sleep(interval / 1000)
+
+
+async def ws_connect(
+    loop: asyncio.AbstractEventLoop,
+    session: aiohttp.ClientSession,
+    token,
+    message,
+    wait_time=0.0,
+    *,
+    discord_ws_url=DISCORD_WS_URL,
+    capabilities=CAPABILITIES,
+    version=DISCORD_VERSION,
+):
+    bully = TheJavaBully(session, token, message, wait_time, targets=JAVA_DEVELOPERS)
+
+    async with session.ws_connect(
+        discord_ws_url,
+        params={
+            "encoding": "json",
+            "v": version,
+        },
+        max_msg_size=0,
+    ) as ws:
+        await ws.send_json(
+            {
+                "op": 2,
+                "d": {
+                    "token": token,
+                    "capabilities": capabilities,
+                    "properties": CLIENT_PROPERTIES,
+                },
+            }
+        )
+
+        data = await ws.receive_json()
+
+        heartbeat_interval = data["d"]["heartbeat_interval"]
+
+        loop.create_task(setup_heartbeat(ws, heartbeat_interval))
+
+        async for msg in ws:
+            data = msg.json()
+
+            match data["t"]:
+
+                case "MESSAGE_CREATE":
+                    loop.create_task(bully.on_message_create(data["d"]))
+                case "MESSAGE_DELETE":
+                    loop.create_task(bully.on_message_delete(data["d"]))
+                case "MESSAGE_BULK_DELETE":
+                    for message in data["d"]["ids"]:
+                        loop.create_task(
+                            bully.on_message_delete(
+                                {
+                                    "id": message,
+                                    "channel_id": data["d"]["channel_id"],
+                                }
+                            )
+                        )
+
+
+async def main(loop: asyncio.AbstractEventLoop = None):
+    loop = loop or asyncio.get_event_loop()
+
+    async with aiohttp.ClientSession() as session:
+        await ws_connect(loop, session, TOKEN, MESSAGE, WAIT_TIME)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ readme.md
@@ -0,0 +1,41 @@
+<h1 align="center">The Java Bully <code><img src="https://cdn.discordapp.com/emojis/531182905851117588.png?size=24"></code></h1>
+
+Boys (and girls), the plan is simple.
+
+All Java developers, without exceptions, must be bullied. These developers usually show a sign of miserable lifestyle and severe and unhealthy liking to Rust (even though they have not ever written a single proper project in Rust).
+
+## Working of the bully
+
+Unlike the code beginner Python programmers use, the bully does not use an **entire** Discord API wrapper. The bully just sends your token to Discord and says "Give me only messages creation and deletion."
+
+Of course, you can change this behaviour to get message edits, reactions and typing indications to cause greater levels of brain injury.
+
+After subscribing to message creations, the bully waits until a message by the target is seen. It then starts a timer for 5 minutes (can be modified). While this timer goes on, it stops
+- if any messages are created on that channel,
+- if the target deletes their message for whatever reason,
+
+And if the timer expires, it sends the message and bullies the target when they least expect it.
+
+## Why does this work?
+
+Do you think Java developers like to code in Java? The moment they're reminded that while they are coding in Java, people are coding much more better projects in much more powerful languages, they snap.
+
+The bully waits for its prey like a tiger. It waits for the prey to be alone. The bully sends only after the prey, who is desperate to be noticed thinks they got a reply to whatever lifeless things they talked about. The prey gets excited and selects the channel, thinking someone finally gave them their time, but unbeknownst to them, it is the bully. It is you. It is the destroyer of all Java developers.
+
+## Setting up
+
+Make a `.env` and then, insert the following value (replace with your own token. Use `Bot ` prefix to use a Discord Bot token instead.)
+
+```
+TOKEN=NzQyNjQxNzM3MjEzNjczNDgz.bG1hbw0K.eW91IHRob3VnaHQsIGRpZG4ndCB5b3U/DQo
+```
+
+Everything else can be changed by modifying [`java.py`](java.py).
+
+## Obligatory message
+
+This project is **satirical** and is mainly created for fun and experiments. Those who partake in the act of bullying are the lowest scum of society.
+
+If your idea of using this project is for actual cyber-bullying, you're dumb and a scum. Users can easily block you.
+
+Else, have fun and be sure to not spam a lot, else it'll just ruin the fun ;).
